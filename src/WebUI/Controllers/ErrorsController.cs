3c76546a499a8cab5fe9ba696a2bb77064aa7967
yiyun <yiyungent@gmail.com>
2019-04-30 15:07:39 +0800

    add 错误页面

--- src/WebUI/Controllers/ErrorsController.cs
+++ src/WebUI/Controllers/ErrorsController.cs
@@ -3,28 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class ErrorsController : Controller
     {
-        public ViewResult WithoutAuth()
+        public ViewResult WithoutAuth(string returnUrl)
         {
-            return View();
+            ErrorRedirectViewModel model = new ErrorRedirectViewModel
+            {
+                Title = "权限不足",
+                Message = "你没有权限访问此页面",
+                RedirectUrl = Url.Action("Index", "Home", new { area = "", returnUrl = returnUrl }),
+                RedirectUrlName = "首页",
+                WaitSecond = 8
+            };
+
+            return View("_ErrorRedirect", model);
         }
 
         public ViewResult NeedLogin(string returnUrl)
         {
-            ViewBag.ReturnUrl = returnUrl;
+            ErrorRedirectViewModel model = new ErrorRedirectViewModel
+            {
+                Title = "请登录",
+                Message = "该页面需要登录",
+                RedirectUrl = Url.Action("Index", "Login", new { area = "Account", returnUrl = returnUrl }),
+                RedirectUrlName = "登录页",
+                WaitSecond = 8
+            };
 
-            return View();
+            return View("_ErrorRedirect", model);
         }
 
         public ViewResult LoginTimeOut(string returnUrl)
         {
-            ViewBag.ReturnUrl = returnUrl;
+            ErrorRedirectViewModel model = new ErrorRedirectViewModel
+            {
+                Title = "请重新登录",
+                Message = "你的登录状态已超时",
+                RedirectUrl = Url.Action("Index", "Login", new { area = "Account", returnUrl = returnUrl }),
+                RedirectUrlName = "登录页",
+                WaitSecond = 8
+            };
 
-            return View();
+            return View("_ErrorRedirect", model);
         }
     }
 }
