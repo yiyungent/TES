695f67cae41fca3a9efcb8c590a9948821b027d9
yiyun <yiyungent@gmail.com>
2019-05-03 10:01:37 +0800

    pjax change to iframe

--- src/WebUI/Areas/Admin/Controllers/HomeController.cs
+++ src/WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using Framework.Infrastructure.Concrete;
 using Framework.Models;
 using Framework.Factories;
 using Framework.Mvc;
+using WebUI.Areas.Admin.Models;
 
 namespace WebUI.Areas.Admin.Controllers
 {
@@ -19,13 +20,21 @@ namespace WebUI.Areas.Admin.Controllers
 
         public HomeController()
         {
-
+            ViewBag.PageHeader = "首页";
+            ViewBag.PageHeaderDescription = "后台管理";
+            ViewBag.BreadcrumbList = new List<BreadcrumbItem>();
         }
 
-        #region 后台主页
+        #region 后台框架
         public ViewResult Index(CurrentAccountModel currentAccount)
         {
+            return View();
+        }
+        #endregion
 
+        #region 后台默认主页
+        public ViewResult Default()
+        {
             return View();
         }
         #endregion
