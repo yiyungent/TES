f2b0953e890d86a6a83cb2bda889530ea634cc3f
yiyun <yiyungent@gmail.com>
2019-05-06 01:58:46 +0800

    add GlobalSharedViewEngine.cs

--- src/Framework/Config/FrameworkConfig.cs
+++ src/Framework/Config/FrameworkConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Framework.Attributes;
 using Framework.Infrastructure.Concrete;
 using Framework.Models;
+using Framework.Mvc.ViewEngine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,7 @@ namespace Framework.Config
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
             RegisterModelBinder(ModelBinders.Binders);
+            RegisterViewEngine(ViewEngines.Engines);
         }
 
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
@@ -33,6 +35,11 @@ namespace Framework.Config
         public static void RegisterModelBinder(ModelBinderDictionary binders)
         {
             binders.Add(typeof(CurrentAccountModel), new CurrentAccountModelBinder());
+        }
+
+        public static void RegisterViewEngine(ViewEngineCollection viewEngines)
+        {
+            viewEngines.Add(new GlobalSharedViewEngine());
         }
     }
 }
---
+++ src/Framework/Mvc/ViewEngine/GlobalSharedViewEngine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Framework.Mvc.ViewEngine
+{
+    public class GlobalSharedViewEngine : RazorViewEngine
+    {
+        public GlobalSharedViewEngine()
+            : this((IViewPageActivator)null)
+        {
+        }
+
+        public GlobalSharedViewEngine(IViewPageActivator viewPageActivator)
+           : base(viewPageActivator)
+        {
+            this.AreaViewLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.AreaMasterLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.AreaPartialViewLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.ViewLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.MasterLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.PartialViewLocationFormats = new[]
+            {
+                "~/GlobalShared/Views/{0}.cshtml"
+            };
+            this.FileExtensions = new[]
+            {
+                "cshtml"
+            };
+        }
+    }
+}
