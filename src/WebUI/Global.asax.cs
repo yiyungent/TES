7a9f20525227b9ff3de81246f369ab8705526bf2
yiyun <yiyungent@gmail.com>
2019-05-18 13:21:57 +0800

    add websocket demo

--- src/WebUI/Global.asax.cs
+++ src/WebUI/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
 using System.Web.Routing;
 using Castle.ActiveRecord;
 using Castle.ActiveRecord.Framework;
@@ -34,6 +35,7 @@ namespace WebUI
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
 
             FrameworkConfig.Register();
         }
