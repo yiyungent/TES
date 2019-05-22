e256426215349550541f7c9271af4d59f96c465e
yiyun <yiyungent@gmail.com>
2019-05-23 01:26:29 +0800

    减少对 jquery的依赖，add 使用原生js替换jquery

--- src/WebUI/dist/pview/pview.js
+++ src/WebUI/dist/pview/pview.js
@@ -40,9 +40,9 @@
 			data: data,
 			dataType: 'html',
 			success: function (data) {
-				// 包装数据
-				var $dataObj = $('<code></code>').append($(data));
-				console.log($dataObj);
+				// html字符串转dom对象
+				var dataObj = document.createElement("code");
+				dataObj.innerHTML = data;
 
 				// 需要更新的区块
 				var updatePviews = [];
@@ -51,21 +51,28 @@
 					updatePviewNames = pview.split(',');
 				}
 				for (var i = 0; i < updatePviewNames.length; i++) {
-					updatePviews[i] = $('[pview="' + updatePviewNames[i] + '"]')[0]; // 注意：每个是一个 js对象
+					// updatePviews[i] = $('[pview="' + updatePviewNames[i] + '"]')[0]; // 注意：每个是一个 js对象
+					updatePviews[i] = document.querySelector('[pview="' + updatePviewNames[i] + '"]');
 				}
-				// 查找当前页面的每一个 pview块, 并将返回的 html 从中筛选出 pview块，将对应的 旧的 pview块替换
+				// 查找当前页面的（点击要求更新的）每一个 区块, 并将返回的 html 从中筛选出 相应区块，将对应的 旧的 区块替换
 				console.log('---------updatePviews-----------');
 				console.log(updatePviews);
+				var pviewItem = null;
 				var pviewItemName = '', pviewItemHtml = '';
 				for (var i = 0; i < updatePviews.length; i++) {
 					// 当前 pview块的 名
-					pviewItemName = $(updatePviews[i]).attr('pview');
-					// 当前 pview块的 新返回的 html
-					pviewItemHtml = $('[pview="' + pviewItemName + '"]', $dataObj).html();
-					console.log('---------updatePviewItem-----------');
-					console.log(updatePviews[i]);
-					updatePviews[i].innerHTML = pviewItemHtml;
-					console.log('------success-----');
+					// pviewItemName = $(updatePviews[i]).attr('pview');
+					pviewItemName = updatePviews[i].getAttribute('pview');
+					// 当前 pview块的 新返回的 区块
+					pviewItem = dataObj.querySelector('[pview="' + pviewItemName + '"]');
+					if (pviewItem != null) {
+						// 返回的 html 中存在此区块，才更新此要求的区块
+						pviewItemHtml = pviewItem.innerHTML;
+						console.log('---------updatePviewItem-----------');
+						console.log(updatePviews[i]);
+						updatePviews[i].innerHTML = pviewItemHtml;
+						console.log('----update success-----');
+					}
 				}
 			}
 		});
