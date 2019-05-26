79a51476db918553b1889c4613aa7719d8348848
yiyun <yiyungent@gmail.com>
2019-05-26 10:00:26 +0800

    add 指标管理列表，编辑

--- src/WebUI/Areas/Admin/Controllers/NormTargetController.cs
+++ src/WebUI/Areas/Admin/Controllers/NormTargetController.cs
@@ -10,16 +10,16 @@ using WebUI.Areas.Admin.Models;
 
 namespace WebUI.Areas.Admin.Controllers
 {
-    public class SysMenuController : Controller
+    public class NormTargetController : Controller
     {
         #region Ctor
-        public SysMenuController()
+        public NormTargetController()
         {
-            ViewBag.PageHeader = "菜单管理";
-            ViewBag.PageHeaderDescription = "菜单管理";
+            ViewBag.PageHeader = "指标管理";
+            ViewBag.PageHeaderDescription = "指标管理";
             ViewBag.BreadcrumbList = new List<BreadcrumbItem>
             {
-                new BreadcrumbItem("系统管理"),
+                new BreadcrumbItem("评价管理"),
             };
         }
         #endregion
@@ -31,33 +31,36 @@ namespace WebUI.Areas.Admin.Controllers
         }
         #endregion
 
-        #region 修改
+        #region 编辑
         [HttpGet]
         public ViewResult Edit(int id)
         {
-            Sys_Menu viewModel = Container.Instance.Resolve<Sys_MenuService>().GetEntity(id);
-            int parentId = viewModel.ParentMenu?.ID ?? 0;
+            NormTarget viewModel = Container.Instance.Resolve<NormTargetService>().GetEntity(id);
+            int parentId = viewModel.ParentTarget?.ID ?? 0;
             ViewBag.DDLParent = InitDDLForParent(viewModel, parentId);
+            //ViewBag.DDLNormType = Init
 
             return View(viewModel);
         }
 
         [HttpPost]
-        public JsonResult Edit(Sys_Menu inputModel)
+        public JsonResult Edit(NormTarget inputModel)
         {
             try
             {
-                Sys_Menu dbModel = Container.Instance.Resolve<Sys_MenuService>().GetEntity(inputModel.ID);
-                // 上级菜单
-                if (inputModel.ParentMenu.ID == 0)
+                NormTarget dbModel = Container.Instance.Resolve<NormTargetService>().GetEntity(inputModel.ID);
+                // 上级
+                if (inputModel.ParentTarget.ID == 0)
                 {
-                    inputModel.ParentMenu = null;
+                    inputModel.ParentTarget = null;
                 }
                 // 设置修改后的值
                 dbModel.Name = inputModel.Name;
                 dbModel.SortCode = inputModel.SortCode;
-                dbModel.ParentMenu = inputModel.ParentMenu;
-                Container.Instance.Resolve<Sys_MenuService>().Edit(dbModel);
+                dbModel.ParentTarget = inputModel.ParentTarget;
+                //dbModel.NormType = inputModel.NormType;
+
+                Container.Instance.Resolve<NormTargetService>().Edit(dbModel);
 
                 return Json(new { code = 1, message = "保存成功" });
             }
@@ -69,17 +72,17 @@ namespace WebUI.Areas.Admin.Controllers
         #endregion
 
         #region Helpers
-        private IList<SelectListItem> InitDDLForParent(Sys_Menu self, int parentId)
+        private IList<SelectListItem> InitDDLForParent(NormTarget self, int parentId)
         {
             IList<SelectListItem> ret = new List<SelectListItem>();
             ret.Add(new SelectListItem()
             {
-                Text = "请选择",
+                Text = "请选择(一级)",
                 Value = "0",
                 Selected = (0 == parentId),
             });
 
-            IList<Sys_Menu> all = Container.Instance.Resolve<Sys_MenuService>().GetAll();
+            IList<NormTarget> all = Container.Instance.Resolve<NormTargetService>().GetAll();
             // 找出自己及后代的ID
             IList<int> idRange = new List<int>();
             GetIdRange(self, idRange, all);
@@ -101,7 +104,7 @@ namespace WebUI.Areas.Admin.Controllers
             // 方案二：考虑层级的实现
             // 1.一级菜单
             var first = from m in find
-                        where m.ParentMenu == null
+                        where m.ParentTarget == null
                         orderby m.SortCode
                         select m;
             foreach (var item in first)
@@ -112,7 +115,7 @@ namespace WebUI.Areas.Admin.Controllers
             return ret;
         }
 
-        private void AddSelfAndChildrenForDDL(string pref, Sys_Menu self, IList<SelectListItem> target, IList<Sys_Menu> all, int parentId)
+        private void AddSelfAndChildrenForDDL(string pref, NormTarget self, IList<SelectListItem> target, IList<NormTarget> all, int parentId)
         {
             // 1.添加自己
             target.Add(new SelectListItem()
@@ -123,7 +126,7 @@ namespace WebUI.Areas.Admin.Controllers
             });
             // 2.递归添加子女
             var child = from m in all
-                        where m.ParentMenu != null && m.ParentMenu.ID == self.ID
+                        where m.ParentTarget != null && m.ParentTarget.ID == self.ID
                         orderby m.SortCode
                         select m;
             foreach (var item in child)
@@ -138,20 +141,23 @@ namespace WebUI.Areas.Admin.Controllers
         /// <param name="self"></param>
         /// <param name="idRange"></param>
         /// <param name="all"></param>
-        private void GetIdRange(Sys_Menu self, IList<int> idRange, IList<Sys_Menu> all)
+        private void GetIdRange(NormTarget self, IList<int> idRange, IList<NormTarget> all)
         {
             // 添加自己
             idRange.Add(self.ID);
 
             // 关于子女循环
             // 第二种解决方法：
-            if (self.Children == null) return;
-            foreach (var item in self.Children)
+            if (self.ChildTargetList == null) return;
+            foreach (var item in self.ChildTargetList)
             {
                 // 递归调用---添加自己和自己的子女
                 GetIdRange(item, idRange, all);
             }
         }
         #endregion
+
+
+
     }
 }
---
+++ src/WebUI/HtmlHelpers/CollapseNormTarget.cs
@@ -0,0 +1,97 @@
+﻿using Core;
+using Domain;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.HtmlHelpers
+{
+    public static class CollapseNormTarget
+    {
+        private static IList<NormTarget> _allList;
+
+        #region 产生树形列表
+        public static MvcHtmlString GenerateCollapseNormTarget(this HtmlHelper value)
+        {
+            IList<NormTarget> allList = Container.Instance.Resolve<NormTargetService>().GetAll();
+            _allList = allList;
+            IList<NormTarget> firstDeptList = (from m in allList
+                                               where m.ParentTarget == null
+                                               orderby m.SortCode ascending
+                                               select m).ToList();
+
+            StringBuilder sbItemHtml = new StringBuilder();
+            // 折叠菜单 layui-collapse
+            sbItemHtml.Append("<div class=\"layui-collapse\" lay-accordion=\"\">");
+
+            // 当前同等级别(一级菜单项) 的  菜单项(许多)
+            foreach (var firstItem in firstDeptList)
+            {
+                // 折叠菜单项 layui-colla-item
+                sbItemHtml.Append("<div class=\"layui-colla-item\">");
+                // 此菜单项标题 layui-colla-title
+                sbItemHtml.AppendFormat("<h2 class=\"layui-colla-title\">{1}<button class='layui-btn layui-btn-sm' onclick='onEdit({0})' style='float: right;'>修改</button></h2>", firstItem.ID, firstItem.Name);
+
+                // 此菜单项内容:  1. 无子项---<p></p>   2. 有子项---折叠菜单
+                sbItemHtml.Append("<div class=\"layui-colla-content\">");
+                // 里面又有许多项----此处开始进入 首次递归----------直到最后某项不再是任何项的父亲，则为 <p></p>
+                // 深度优先--- 利用循环拿到每个菜单项,再进递归拿取其子菜单
+                // 注意：如果当前菜单项已经无子项，则为 <p></p>，否则  继续向里递归寻找
+                if (firstItem.ChildTargetList == null || firstItem.ChildTargetList.Count == 0)
+                {
+                    sbItemHtml.AppendFormat("<p>{0} 无子项</p>", firstItem.Name);
+                }
+                else
+                {
+                    // 否则又是一个折叠菜单
+                    SubItemList(ref sbItemHtml, firstItem);
+                }
+
+                sbItemHtml.Append("</div>");
+
+                sbItemHtml.Append("</div>");
+            }
+
+            sbItemHtml.Append("</div>");
+
+            return MvcHtmlString.Create(sbItemHtml.ToString());
+        }
+        #endregion
+
+        #region 递归查找子项
+        private static void SubItemList(ref StringBuilder sbItemHtml, NormTarget currentItem)
+        {
+            IList<NormTarget> suDeptList = (from m in _allList
+                                            where m.ParentTarget != null && m.ParentTarget.ID == currentItem.ID
+                                            orderby m.SortCode ascending
+                                            select m).ToList();
+            // 此菜单项下又嵌套一个折叠菜单
+            sbItemHtml.Append("<div class=\"layui-collapse\" lay-accordion=\"\">");
+            foreach (var item in suDeptList)
+            {
+                sbItemHtml.Append("<div class=\"layui-colla-item\">");
+                sbItemHtml.AppendFormat("<h2 class=\"layui-colla-title\">{1}<button class='layui-btn layui-btn-sm' onclick='onEdit({0})' style='float: right;'>修改</button></h2>", item.ID, item.Name);
+
+                sbItemHtml.Append("<div class=\"layui-colla-content\">");
+                // 注意：如果当前菜单项已经无子项，则为 <p></p>，否则  继续向里递归寻找
+                if (item.ChildTargetList == null || item.ChildTargetList.Count == 0)
+                {
+                    sbItemHtml.AppendFormat("<p>{0} 无子项</p>", item.Name);
+                }
+                else
+                {
+                    SubItemList(ref sbItemHtml, item);
+                }
+                sbItemHtml.Append("</div>");
+
+                sbItemHtml.Append("</div>");
+            }
+            sbItemHtml.Append("</div>");
+        }
+        #endregion
+    }
+}
