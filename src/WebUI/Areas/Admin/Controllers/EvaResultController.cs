0f4f0372f828beae46c041946308171f5907a8ed
yiyun <yiyungent@gmail.com>
2019-06-06 12:01:25 +0800

    add 导出某评价任务的评价结果excel

--- src/WebUI/Areas/Admin/Controllers/EvaResultController.cs
+++ src/WebUI/Areas/Admin/Controllers/EvaResultController.cs
@@ -1,9 +1,11 @@
-﻿using Core;
+﻿using Common;
+using Core;
 using Domain;
 using NHibernate.Criterion;
 using Service;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -257,6 +259,73 @@ namespace WebUI.Areas.Admin.Controllers
         }
         #endregion
 
+        #region 导出某评价任务的评价结果Excel
+        public JsonResult ExportExcel(int evaTaskId)
+        {
+            try
+            {
+                EvaResultListViewModel viewModel = new EvaResultListViewModel();
+                IList<EvaResult> allEvaResult = Container.Instance.Resolve<EvaResultService>().GetAll();
+                // 去重 + 只要有评价结果记录的
+
+                IList<EvaTask> allRelativeEvaTask = allEvaResult.Select(m => m.EvaluateTask).Distinct().ToList();
+                IList<EmployeeInfo> allEvaedEmployee = allEvaResult.Select(m => m.Teacher).Distinct().ToList();
+
+                IList<EvaTask> allEvaTask = Container.Instance.Resolve<EvaTaskService>().GetAll();
+                IList<EmployeeInfo> allEmployee = Container.Instance.Resolve<EmployeeInfoService>().GetAll();
+
+                IList<NormType> allNormType = Container.Instance.Resolve<NormTypeService>().GetAll();
+
+                viewModel.List = new List<EvaResultVMItem>();
+                foreach (var evaedEmployeeItem in allEmployee)
+                {
+                    foreach (var evaTask in allEvaTask)
+                    {
+                        EvaResultVMItem vmItem = new EvaResultVMItem();
+                        vmItem.EvaedEmployee = evaedEmployeeItem;
+                        vmItem.EvaTask = evaTask;
+                        vmItem.ScoreDic = new Dictionary<NormType, decimal>();
+                        // 只要 此评价任务，此被评员工 的评价记录
+                        // 注意：表头的评价类型是按 排序码 排序，所以下方的明细分数单元格也许按排序码排序，以达明细分数一一对应
+                        IList<EvaResult> relativeResultList = allEvaResult.Where(m => m.EvaluateTask.ID == vmItem.EvaTask.ID && m.Teacher.ID == vmItem.EvaedEmployee.ID).OrderBy(m => m.NormType.SortCode).ToList();
+                        foreach (var normType in allNormType)
+                        {
+                            vmItem.ScoreDic.Add(normType, relativeResultList.Where(m => m.NormType.ID == normType.ID).Select(m => m.Score).FirstOrDefault());
+                        }
+                        if (!viewModel.List.Contains(vmItem, new EvaResultVMItemCompare()))
+                        {
+                            viewModel.Add(vmItem);
+                        }
+                    }
+                }
+
+                // 筛选-只要此评价任务
+                viewModel.List = viewModel.List.Where(m => m.EvaTask.ID == evaTaskId).ToList();
+
+                IList<EvaResultVMItem> excelDataList = viewModel.List;
+
+                EvaTask currentEvaTask = allEvaTask.Where(m => m.ID == evaTaskId).FirstOrDefault();
+                string saveFilePath = "/Resources/Excels/" + currentEvaTask.Name + "评价情况表.xls";
+                if (!System.IO.File.Exists(Server.MapPath(saveFilePath)))
+                {
+                    System.IO.File.Create(Server.MapPath(saveFilePath)).Dispose();
+                }
+
+                // 将数据列表转换为 DataTable
+                DataTable dataTable = EvaResultVMIListToDataTable(excelDataList);
+                // 将 DataTable 保存到本地，返回文件路径
+                NPOIHelper.DataTableToExcel(Server.MapPath(saveFilePath), dataTable, currentEvaTask.Name + "评价情况表", true);
+
+                return Json(new { code = 1, message = "导出成功", fileUrl = saveFilePath }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { code = -1, message = "导出失败", fileUrl = "" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        #endregion
 
 
 
@@ -424,6 +493,38 @@ namespace WebUI.Areas.Admin.Controllers
             {
                 throw new NotImplementedException();
             }
+        }
+        #endregion
+
+        #region 数据列表转换为 DataTable
+        private DataTable EvaResultVMIListToDataTable(IList<EvaResultVMItem> excelDataList)
+        {
+            DataTable rtn = new DataTable();
+            rtn.Columns.Add("评价任务");
+            rtn.Columns.Add("被评价教师（员工）");
+            IList<NormType> allNormType = Container.Instance.Resolve<NormTypeService>().GetAll().OrderBy(m => m.SortCode).ToList();
+            foreach (var normType in allNormType)
+            {
+                rtn.Columns.Add(normType.Name, typeof(decimal));
+            }
+            rtn.Columns.Add("总分");
+
+            foreach (var item in excelDataList)
+            {
+                IList<object> rowDatas = new List<object>();
+                rowDatas.Add(item.EvaTask.Name);
+                rowDatas.Add(item.EvaedEmployee.Name + "（" + item.EvaedEmployee.EmployeeCode + "）");
+                // 循环添加 各评价类型分数，注意与表头的一一对应
+                foreach (var normTypeAndScore in item.ScoreDic)
+                {
+                    rowDatas.Add(normTypeAndScore.Value);
+                }
+                rowDatas.Add(item.ScoreSum);
+
+                rtn.Rows.Add(rowDatas.ToArray());
+            }
+
+            return rtn;
         }
         #endregion
 
