5a8c18d6228585e892b5263d10af27eaab77a570
yiyun <yiyungent@gmail.com>
2019-06-08 21:13:38 +0800

    change Plugin.Contents

---
+++ Plugins/Plugin.Contents/Controllers/ShowContentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plugin.Contents.Controllers
+{
+    public class ShowContentsController : Controller
+    {
+        // GET: ShowContents
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
